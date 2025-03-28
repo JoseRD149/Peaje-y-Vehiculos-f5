54d3bbe0c179c60b5ee6a8d1003c54f7284faf7e
JoseRD149 <joseromerodiaz89@gmail.com>
2025-03-28 10:23:14 +0100

    ClassTollstation

---
+++ peaje/src/main/java/peaje/TollStation.java
@@ -0,0 +1,33 @@
+package peaje;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TollStation {
+    private String name;
+    private String city;
+    private int totalCollected;
+    private List<Vehicle> vehiclesList;
+
+    public TollStation(String name,String city){
+        this.name = name;
+        this.city = city;
+        this.totalCollected = 0;
+        this.vehiclesList = new ArrayList<>();
+
+    }
+    public void registerVehicle(Vehicle vehicle) {
+        vehiclesList.add(vehicle);
+        totalCollected += vehicle.getTollFee();
+    }
+    
+    public int getTotallToll(){
+        return totalCollected;
+    }
+    public void printTollSummary() {
+        System.out.println("Toll Station: " + name + " - " + city);
+        for (Vehicle v : vehiclesList) {
+            System.out.println("Vehicle: " + v.getLicensePlate() + " | Toll: $" + v.getTollFee());
+        }
+        System.out.println("Total Collected: $" + totalCollected);
+    }
+}
