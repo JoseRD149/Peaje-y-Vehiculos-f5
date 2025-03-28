503cf03ac5d1f3ab55789152727845c40d37a0db
JoseRD149 <joseromerodiaz89@gmail.com>
2025-03-28 08:45:39 +0100

    classvehicle

---
+++ peaje/src/main/java/peaje/Vehicle.java
@@ -0,0 +1,14 @@
+package peaje;
+
+abstract class Vehicle{
+
+    protected String licensePlate;
+
+    public Vehicle(String licensePlate){
+        this.licensePlate = licensePlate;
+    }
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+    public abstract double getTollFee();
+}
